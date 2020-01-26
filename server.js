bc0fa2bac081eef78f7a7691bb68b087a8f14fe7
Kellie Hunsaker <kellie13060@gmail.com>
2020-01-25 23:12:38 -0700

    beautify files

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 //setting up express to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
 
@@ -16,6 +18,6 @@ require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
 //starting the server
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
 });
